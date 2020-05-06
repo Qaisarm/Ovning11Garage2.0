3cc58f98d55abb8fff620dcfbfd0c24239eec850
Qaisarm <qmphy99@gmail.com>
2020-05-06 17:11:04 +0200

    Migration Added for New column

--- Ovning11Garage2.0/Migrations/20200506141104_WheelProperty.Designer.cs
+++ Ovning11Garage2.0/Migrations/20200506141104_WheelProperty.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ovning11Garage2._0.Data;
 
 namespace Ovning11Garage2._0.Migrations
 {
     [DbContext(typeof(Ovning11Garage2_0Context))]
-    partial class Ovning11Garage2_0ContextModelSnapshot : ModelSnapshot
+    [Migration("20200506141104_WheelProperty")]
+    partial class WheelProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ovning11Garage2.0/Migrations/20200506141104_WheelProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ovning11Garage2._0.Migrations
+{
+    public partial class WheelProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumnOfWheels",
+                table: "ParkedVehicle",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumnOfWheels",
+                table: "ParkedVehicle");
+        }
+    }
+}
