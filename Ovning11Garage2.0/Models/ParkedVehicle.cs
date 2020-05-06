ec06732982471ba46d7e0167867bd618184081b6
Qaisarm <qmphy99@gmail.com>
2020-05-06 12:13:34 +0200

    Added ParkedVehicle Model

---
+++ Ovning11Garage2.0/Models/ParkedVehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ovning11Garage2._0.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+       
+        public VehicleType VehicleType { get; set; }
+     
+        public string RegistrationNumber { get; set; }
+        public String Color { get; set; }
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        
+
+        //Arival Time
+        public DateTime TimeOfParking { get; set; }
+    }
+
+    public enum VehicleType
+    {
+        Car,
+        Boat,
+        Buss,
+        Airplane,
+        Cycle
+    }
+}
