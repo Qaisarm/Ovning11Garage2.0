8efd0520a257ad33eb4ac808465c9f1b44263936
Qaisarm <qmphy99@gmail.com>
2020-05-06 12:20:26 +0200

    Added Parked Vehicles Controller

---
+++ Ovning11Garage2.0/Data/Ovning11Garage2_0Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ovning11Garage2._0.Models;
+
+namespace Ovning11Garage2._0.Data
+{
+    public class Ovning11Garage2_0Context : DbContext
+    {
+        public Ovning11Garage2_0Context (DbContextOptions<Ovning11Garage2_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ovning11Garage2._0.Models.ParkedVehicle> ParkedVehicle { get; set; }
+    }
+}
