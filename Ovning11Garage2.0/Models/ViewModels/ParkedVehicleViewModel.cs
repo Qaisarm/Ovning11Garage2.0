bca256407ecc79851367ca58759284519cf52a00
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-08 11:42:46 +0200

    Adding Overview Viewmodel

---
+++ Ovning11Garage2.0/Models/ViewModels/ParkedVehicleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Ovning11Garage2._0.Models.ViewModels
+{
+    public class ParkedVehicleViewModel
+    {
+        public int Id { get; set; }
+
+        public VehicleType VehicleType { get; set; }
+
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+
+        //Arival Time
+        public DateTime TimeOfParking { get; set; }
+
+        public ParkedVehicleViewModel()
+        {
+        }
+    }
+}
