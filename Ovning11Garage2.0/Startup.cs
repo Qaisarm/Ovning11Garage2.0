b27c6912a2eaf146850395329f353ebf2e6d32e0
Qaisarm <qmphy99@gmail.com>
2020-05-08 13:52:37 +0200

    Changed names of fields

--- Ovning11Garage2.0/Startup.cs
+++ Ovning11Garage2.0/Startup.cs
@@ -55,7 +55,7 @@ namespace Ovning11Garage2._0
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
