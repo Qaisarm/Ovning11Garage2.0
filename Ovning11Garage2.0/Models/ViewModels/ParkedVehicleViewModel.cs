5aab5e60918d7d196306fbbfa156f8b46e0fcac2
Qaisarm <qmphy99@gmail.com>
2020-05-10 00:48:24 +0200

    Display Data Annotation for Date is added

--- Ovning11Garage2.0/Models/ViewModels/ParkedVehicleViewModel.cs
+++ Ovning11Garage2.0/Models/ViewModels/ParkedVehicleViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Ovning11Garage2._0.Models.ViewModels
 {
     public class ParkedVehicleViewModel
@@ -11,6 +13,7 @@ namespace Ovning11Garage2._0.Models.ViewModels
         public string Color { get; set; }
 
         //Arival Time
+        [DisplayFormat(DataFormatString = "{0:f}")]
         public DateTime TimeOfParking { get; set; }
 
         public ParkedVehicleViewModel()
